71730f2574b1fb6f7f9fd24b0ea88e144b1cf11a
Ryan W <rywais@gmail.com>
2018-05-18 14:51:00 -0700

    Code fixes

--- time_to_heat.py
+++ time_to_heat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-SERIAL_PORT = '/dev/ttyUSB0'
+SERIAL_PORT = '/dev/ttyUSB1'
 LOWER_TEMP = 100
 UPPER_TEMP = 110
 STABILITY_TOL = 0.15
@@ -7,9 +7,10 @@ STABILITY_TIME = 15
 
 import e5cc as e
 import time as t
+import serial
 
 try:
-  ser = e.init_serial('/dev/ttyUSB0')
+  ser = e.init_serial(SERIAL_PORT)
 except serial.serialutil.SerialException:
   print('Error initializing Serial Port')
   exit()
@@ -44,7 +45,7 @@ ramp_start = t.perf_counter()
 pv = LOWER_TEMP
 
 #Wait to reach upper temperature threshold
-while pv < UPPER_TEMP-0.01
+while (pv < UPPER_TEMP - 0.01):
   pv = e.read_temp(ser)
 
 #Measure time elapsed and turn the temperature back down
@@ -53,4 +54,4 @@ e.set_pid(ser)
 e.set_temp(UPPER_TEMP)
 
 print('Time to ramp temperature from %3.1f to %3.1f is %.2f seconds' \
-  % (LOWER_TEMP,UPPER_TEMP,ramp_finish-ramp_start)
+  % (LOWER_TEMP,UPPER_TEMP,ramp_finish-ramp_start))
