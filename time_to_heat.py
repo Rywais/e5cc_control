e3ccda1435fbf20d490393ee6e663c45f4de2032
Ryan W <rywais@gmail.com>
2018-05-18 20:26:51 -0700

    Graphing capability added

--- time_to_heat.py
+++ time_to_heat.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 SERIAL_PORT = '/dev/ttyUSB1'
-LOWER_TEMP = 100
-UPPER_TEMP = 110
+LOWER_TEMP = 80
+UPPER_TEMP = 120
 STABILITY_TOL = 0.15
 STABILITY_TIME = 15
 
 import e5cc as e
 import time as t
 import serial
+import numpy as np
+import matplotlib.pyplot as plt
 
 try:
   ser = e.init_serial(SERIAL_PORT)
@@ -43,10 +45,15 @@ e.set_onoff(ser)
 e.set_temp(UPPER_TEMP+5, ser)
 ramp_start = t.perf_counter()
 pv = LOWER_TEMP
+temp_array = np.array([])
+time_array = np.array([])
 
 #Wait to reach upper temperature threshold
 while (pv < UPPER_TEMP - 0.01):
   pv = e.read_temp(ser)
+  temp_array = np.append(temp_array, pv)
+  time_array = np.append(time_array, t.perf_counter() - ramp_start)
+  
 
 #Measure time elapsed and turn the temperature back down
 ramp_finish = t.perf_counter()
@@ -55,3 +62,8 @@ e.set_temp(UPPER_TEMP, ser)
 
 print('Time to ramp temperature from %3.1f to %3.1f is %.2f seconds' \
   % (LOWER_TEMP,UPPER_TEMP,ramp_finish-ramp_start))
+
+plt.plot(time_array,temp_array)
+plt.ylabel('Temperature (deg Celcius)')
+plt.xlabel('Time (Seconds)')
+plt.show()
